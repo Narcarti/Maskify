24bc6a9b5437571cf9a81e9f0062a8e99b468575
Narcarti <tcmarkymark@gmail.com>
2019-10-29 15:04:52 -0500

    Add files via upload

---
+++ Maskify/Maskify/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maskify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Maskify("444"));
+            Console.WriteLine(Maskify("4445556666"));
+
+            Console.ReadKey();
+        }
+
+        private static string Maskify(string cc)
+        {
+
+            return (cc.Length > 4 ? string.Concat(new string('#',cc.Length-4), cc.Substring(cc.Length - 4)) : cc);
+        }
+    }
+}
